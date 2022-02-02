7bd72e395691188893f847ff564c2354e141a246
Tej Patel <tej.patel@excellentwebworld.in>
2022-02-02 16:56:25 +0530

    Nunito Font Added

--- Curbside Delivery/Utilities/FontBook.swift
+++ Curbside Delivery/Utilities/FontBook.swift
@@ -21,30 +21,60 @@ enum FontSize : CGFloat {
 
 enum FontBook {
     
-    case regular, light, black, blackItalic, bold, boldItalic, book, bookItalic, lightitalic, thin, thinItalic
+    case Nexaregular, Nexalight, Nexablack, NexablackItalic, Nexabold, NexaboldItalic, Nexabook, NexabookItalic, Nexalightitalic, Nexathin, NexathinItalic,
+         regular, light, black, blackItalic, bold, boldItalic, lightitalic, semiBold , book , bookItalic, thin , thinItalic , semiBoldItalic
     func font(ofSize fontSize: CGFloat) -> UIFont {
         switch self{
         case .regular:
-            return UIFont(name: "Nexa-Regular", size: fontSize)!
+            return UIFont(name: "Nunito-Regular", size: fontSize)!
         case .light:
-            return UIFont(name: "Nexa-Light", size: fontSize)!
+            return UIFont(name: "Nunito-Light", size: fontSize)!
         case .black:
-            return UIFont(name: "Nexa-Black", size: fontSize)!
+            return UIFont(name: "Nunito-Black", size: fontSize)!
         case .blackItalic:
-            return UIFont(name: "Nexa-BlackItalic", size: fontSize)!
+            return UIFont(name: "Nunito-BlackItalic", size: fontSize)!
         case .bold:
-            return UIFont(name: "Nexa-Black", size: fontSize)!
+            return UIFont(name: "Nunito-Bold", size: fontSize)!
         case .boldItalic:
-            return UIFont(name: "Nexa-BlackItalic", size: fontSize)!
-        case .book:
-            return UIFont(name: "Nexa-Book", size: fontSize)!
-        case .bookItalic:
-            return UIFont(name: "Nexa-BookItalic", size: fontSize)!
+            return UIFont(name: "Nunito-BoldItalic", size: fontSize)!
         case .lightitalic:
-            return UIFont(name: "Nexa-LightItalic", size: fontSize)!
+            return UIFont(name: "Nunito-LightItalic", size: fontSize)!
+        case .semiBold:
+            return UIFont(name: "Nunito-SemiBold", size: fontSize)!
+        case .semiBoldItalic:
+              return UIFont(name: "Nunito-SemiBoldItalic", size: fontSize)!
+        case .book:
+            return UIFont(name: "Nexa-Book", size: fontSize)! //Not available in Nunito
+        case .bookItalic:
+            return UIFont(name: "Nexa-BookItalic", size: fontSize)!  //Not available in Nunito
         case .thin:
-            return UIFont(name: "Nexa-Thin", size: fontSize)!
+            return UIFont(name: "Nexa-Thin", size: fontSize)! //Not available in Nunito
         case .thinItalic:
+            return UIFont(name: "Nexa-ThinItalic", size: fontSize)! //Not available in Nunito
+            
+            
+            
+        case .Nexaregular:
+            return UIFont(name: "Nexa-Regular", size: fontSize)!
+        case .Nexalight:
+            return UIFont(name: "Nexa-Light", size: fontSize)!
+        case .Nexablack:
+            return UIFont(name: "Nexa-Black", size: fontSize)!
+        case .NexablackItalic:
+            return UIFont(name: "Nexa-BlackItalic", size: fontSize)!
+        case .Nexabold:
+            return UIFont(name: "Nexa-Black", size: fontSize)!
+        case .NexaboldItalic:
+            return UIFont(name: "Nexa-BlackItalic", size: fontSize)!
+        case .Nexabook:
+            return UIFont(name: "Nexa-Book", size: fontSize)!
+        case .NexabookItalic:
+            return UIFont(name: "Nexa-BookItalic", size: fontSize)!
+        case .Nexalightitalic:
+            return UIFont(name: "Nexa-LightItalic", size: fontSize)!
+        case .Nexathin:
+            return UIFont(name: "Nexa-Thin", size: fontSize)!
+        case .NexathinItalic:
             return UIFont(name: "Nexa-ThinItalic", size: fontSize)!
         }
     }
